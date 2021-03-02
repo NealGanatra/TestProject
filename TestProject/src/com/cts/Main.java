7b3f0fca0bd1925661cd4c3bdce6717dc6b55185
Neel <neelg817@gmail.com>
2021-03-03 00:29:41 +0530

    Added User class

--- TestProject/src/com/cts/Main.java
+++ TestProject/src/com/cts/Main.java
@@ -1,7 +1,20 @@
 package com.cts;
 
+import com.model.User;
+
 public class Main {
 public static void main(String[] args) {
 	System.out.println("hello World!!");
+	User u1=new User(1,"hey");
+	User u2=new User(1,"heyl");
+	
+	if(u1.equals(u2)) {
+		System.out.println("Same");
+	}
+	else
+		System.out.println("Different");
+	
+	System.out.println("U1-->"+u1+"\t U2-->"+u2);
 }
+
 }
